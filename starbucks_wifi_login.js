e9be07b0f0d9abf75e71c932a8834a8ac31e72ff
Genki OHMORI <0930019@gmail.com>
2017-06-22 22:06:11 +0900

    Fix

--- starbucks_wifi_login.js
+++ starbucks_wifi_login.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 const Nightmare = require('nightmare');
 const nightmare = Nightmare({ show: false });
@@ -7,16 +7,21 @@ const url = 'https://service.wi2.ne.jp/wi2auth/at_STARBUCKS_Wi2/index.html'
 nightmare
   .goto(url)
   .wait('#button_next_page')
+  .screenshot('/vagrant/1.png')
   .click('#button_next_page')
   .wait('#button_accept')
+  .screenshot('/vagrant/2.png')
   .click('#button_accept')
+  .wait(3000)
+  .screenshot('/vagrant/3.png')
   .evaluate(function () {
-    return 1
+    return document.location.href;
   })
   .end()
   .then(function (result) {
-    console.log(result);
+      return result;
   })
   .catch(function (error) {
+    nightmare.screenshot('/vagrant/4.png')
     console.error('Search failed:', error);
   });
