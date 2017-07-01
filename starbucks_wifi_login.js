a0637401149772f46880fa5de8ddd7caa3d96f11
Genki OHMORI <0930019@gmail.com>
2017-07-01 20:14:16 +0900

    Check success or fail

--- starbucks_wifi_login.js
+++ starbucks_wifi_login.js
@@ -7,21 +7,23 @@ const url = 'https://service.wi2.ne.jp/wi2auth/at_STARBUCKS_Wi2/index.html'
 nightmare
   .goto(url)
   .wait('#button_next_page')
-  .screenshot('/vagrant/1.png')
   .click('#button_next_page')
   .wait('#button_accept')
-  .screenshot('/vagrant/2.png')
   .click('#button_accept')
-  .wait(3000)
-  .screenshot('/vagrant/3.png')
   .evaluate(function () {
     return document.location.href;
   })
   .end()
-  .then(function (result) {
-      return result;
+  .then(function (url) {
+    if (url == 'http://www.starbucks.co.jp/') {
+      console.log('success');
+    }
+    else {
+      console.log('fail');
+      console.log(url);
+    }
   })
   .catch(function (error) {
-    nightmare.screenshot('/vagrant/4.png')
+    nightmare.screenshot('/vagrant/error.png')
     console.error('Search failed:', error);
   });
