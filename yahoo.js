da2ef392725c737224a61cd807586f6819ca0e49
Genki OHMORI <0930019@gmail.com>
2017-07-20 21:15:12 +0900

    Update

--- yahoo.js
+++ yahoo.js
@@ -5,35 +5,48 @@ const Nightmare = require('nightmare');
 const nightmare = Nightmare({show: false});
 const fs = require('fs');
 
-const URL = 'https://store.shopping.yahoo.co.jp/mrmax/4580354188037.html';
+const URL = 'https://store.shopping.yahoo.co.jp/soukaidrink/9000009987990.html';
 
 let info = fs.readFileSync(ACCOUNT_INFO_PLACE, 'utf8').split(/\n/);
 
+// 商品をカートに入れる
+const add_item_to_the_cart_button = '#ItemInfo > div.gdColumns.dvw300.cf > div.gdColumnRight > form > div.mdItemInfoCartButton > p.elWrpButton > a'
+// ログインしてご注文手続きへ
+const goto_login_button = '#item > div > form > div > div.dvSum > table > tbody > tr > td > table > tbody > tr > td.dcSumBtn > ul > li:nth-child(2) > div > input'
+// 次へ
+const next_button = '#btnNext'
+// ログイン
+const login_button = '#btnSubmit'
+// ご注文内容の確認
+const review_order_button = '#toReview'
+// ご注文を確定
+const confirm_order_button = '#form > div:nth-child(4) > div.mdEnterButton.dcMl15 > input'
+
 nightmare
-  .viewport(3200, 3200)
   .goto(URL)
-  .wait('#ItemInfo > div.gdColumns.dvw300.cf > div.gdColumnRight > form > div.mdItemInfoCartButton > p.elWrpButton > a')
+  .wait(add_item_to_the_cart_button)
   .wait(500)
-  .click('div#ItemInfo > div.gdColumns.dvw300.cf:nth-child(2) > div.gdColumnRight:nth-child(2) > form:nth-child(9) > div.mdItemInfoCartButton:nth-child(6) > p.elWrpButton:nth-child(3) > a.elCartButton.elButton:nth-child(1) > span:nth-child(1)')
-  .wait('#item > div > form > div > div.dvSum > table > tbody > tr > td > table > tbody > tr > td.dcSumBtn > ul > li:nth-child(2) > div > input')
+  .click(add_item_to_the_cart_button)
+  .wait(goto_login_button)
   .wait(500)
-  .click('div#item > div.mdOrderDetail:nth-child(1) > form.basketForm:nth-child(1) > div.dvOrderDetailWrapper:nth-child(2) > div.dvSum:nth-child(2) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td.dcSumOthers:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td.dcSumBtn:nth-child(3) > ul.elSumSubmit:nth-child(1) > li:nth-child(2) > div.mdEnterButton.dcEnterBtnThin:nth-child(1) > input.dcEnterButton.dcMeiryo:nth-child(1)')
-  .wait('#btnNext')
+  .click(goto_login_button)
+  .wait(next_button)
   .wait(500)
   .type('#username', info[0])
-  .click('#btnNext')
-  .wait('#btnSubmit')
+  .click(next_button)
+  .wait(login_button)
   .wait(500)
   .type('#passwd', info[1])
-  .click('button#btnSubmit')
-  .wait('#orderSubmit')
+  .click(login_button)
+  .wait(review_order_button)
   .wait(500)
-  .click('input#toReview')
-  //.wait('#form > div:nth-child(4) > div.mdEnterButton.dcMl15 > input')
-  .wait(5000)
-  .screenshot('/vagrant/test6.png')
+  .click(review_order_button)
+  .wait(confirm_order_button)
+  .evaluate(() => {
+    return document.location.href;
+  })
   .end()
-  .then(function (result) {
+  .then((result) => {
     console.log(result)
   })
   .catch(function (error) {
